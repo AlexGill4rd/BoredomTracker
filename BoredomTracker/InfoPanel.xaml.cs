3e625a5aacb871c616a2b568ca5a07401b726082
Alex <alexgillard04@gmail.com>
2022-08-19 16:17:59 +0200

    Added many features to the application
    - Added start/stop
    - Added info tab
    - Added variable to see how long your bored
    - Variable to see total bored time
    -Variable to see how much you earned when bored
    
    To add:
    - Edit money/hour

---
+++ BoredomTracker/InfoPanel.xaml.cs
@@ -0,0 +1,124 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace BoredomTracker
+{
+    /// <summary>
+    /// Interaction logic for InfoPanel.xaml
+    /// </summary>
+    public partial class InfoPanel : Window
+    {
+        DispatcherTimer dispatcherTimer = new DispatcherTimer();
+        DateTime boredomStarted;
+        TimeSpan currentBoredom = new TimeSpan(0, 0, 0);
+        TimeSpan totalBoredom = new TimeSpan(0, 0, 0);
+        TimeSpan newTotalBoredom = new TimeSpan(0, 0, 0);
+        public bool isBored = false;
+        float salary = 13;
+        public InfoPanel()
+        {
+            InitializeComponent();
+            dispatcherTimer.Tick += new EventHandler(dispatcherTimer_Tick);
+            dispatcherTimer.Interval = new TimeSpan(0, 0, 1);
+            LoadData();
+        }
+        private void LoadData()
+        {
+            if (File.Exists("boredom.txt"))
+            {
+                string[] lines = File.ReadAllLines(@"boredom.txt");
+                foreach (string line in lines)
+                {
+                    JObject jsonObject = JObject.Parse(line);
+                    totalBoredom = TimeSpan.Parse((string)jsonObject["TotalBoredom"]);
+                    newTotalBoredom = totalBoredom;
+                    isBored = (bool)jsonObject["Bored"];
+                    if (isBored)
+                    {
+                        boredomStarted = (DateTime)jsonObject["BoredomStarted"];
+                        dispatcherTimer.Start();
+                        newTotalBoredom += DateTime.Now.Subtract(boredomStarted);
+                    }
+                    UpdateData();
+                }
+            }
+        }
+
+        private void btnClose_Click(object sender, RoutedEventArgs e)
+        {
+            base.Hide();
+        }
+        private void dispatcherTimer_Tick(object sender, EventArgs e)
+        {
+            UpdateData();
+        }
+        public void UpdateData()
+        {
+            currentBoredom = DateTime.Now.Subtract(boredomStarted);
+            newTotalBoredom += TimeSpan.FromSeconds(1);
+            if (isBored)
+            {
+                txtCurrentBoredom.Text = currentBoredom.ToString(@"dd\.hh\:mm\:ss");
+            }
+            txtTotalBoredom.Text = newTotalBoredom.ToString(@"dd\.hh\:mm\:ss");
+
+            if (isBored) lblBored.Content = "Currently: BORED";
+            else lblBored.Content = "Currently: NOT BORED";
+
+            float money = ((float)newTotalBoredom.Hours * salary) + ((float)newTotalBoredom.Minutes / 60 * salary) + ((float)newTotalBoredom.Seconds / 60 / 60 * salary);
+            lblFreeMoney.Content = "€" + money.ToString("0.00");
+        }
+        public void Start()
+        {
+            isBored = true;
+            boredomStarted = DateTime.Now;
+            dispatcherTimer.Start();
+        }
+        public void Stop()
+        {
+            totalBoredom += DateTime.Now.Subtract(boredomStarted);
+            isBored = false;
+            dispatcherTimer.Stop();
+            lblBored.Content = "Currently: NOT BORED";
+            Save();
+        }
+        public void Save()
+        {
+            var path = "boredom.txt";
+            var data = new
+            {
+                TotalBoredom = totalBoredom,
+                Bored = isBored,
+                BoredomStarted = boredomStarted,
+            };
+            string jsonData = JsonConvert.SerializeObject(data);
+            File.WriteAllText(path, jsonData);
+        }
+
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+                this.DragMove();
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            Save();
+        }
+    }
+}
--- BoredomTracker/MainWindow.xaml.cs
+++ BoredomTracker/MainWindow.xaml.cs
@@ -20,9 +20,49 @@ namespace BoredomTracker
     /// </summary>
     public partial class MainWindow : Window
     {
+        InfoPanel info = new InfoPanel();
         public MainWindow()
         {
             InitializeComponent();
+            if (info.isBored)
+            {
+                btnStop.IsEnabled = true;
+                btnStart.IsEnabled = false;
+            }
+            else
+            {
+                btnStop.IsEnabled = false;
+                btnStart.IsEnabled = true;
+            }
+        }
+
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+                this.DragMove();
+        }
+
+        private void btnClose_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void btnStart_Click(object sender, RoutedEventArgs e)
+        {
+            btnStop.IsEnabled = true;
+            btnStart.IsEnabled = false;
+            info.Start();
+        }
+        private void btnStop_Click(object sender, RoutedEventArgs e)
+        {
+            btnStop.IsEnabled = false;
+            btnStart.IsEnabled = true;
+            info.Stop();
+        }
+
+        private void btnInfo_Click(object sender, RoutedEventArgs e)
+        {
+            info.Show();
         }
     }
 }
